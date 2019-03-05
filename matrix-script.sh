a74412c071d0f1a514274127a7a7dc1524dfca4d
susloparov <mvsusloparov@edu.hse.ru>
2019-03-05 23:52:18 +0300

    Added core restriction (with taskset), changed initial set of matrix dimensions

--- matrix-script.sh
+++ matrix-script.sh
@@ -1,15 +1,17 @@
-matDims=( 3 5 10 20 50 75 100 200 300 400 500 600 700 800 900 1000 )
-tries=3
-cycles=200
+#matDims=( 3 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100 )
+matDims=100
+tries=5
+cycles=10000
 
-for matDim in "${matDims[@]}"
+#for matDim in "${matDims[@]}"
+for ((matDim=0; matDim<$matDims; matDim++))
 do
-    for (( i=0; i<$tries; i++ ))
+    for (( try=0; try<$tries; try++ ))
     do
-        echo "start $(date) $matDim $i" >> matrix-timelog.txt
-        echo ""$matDim"x"$matDim" matrix, try $i"
-        ./matrix-c.o $matDim $cycles
-        echo "start $(date) $matDim $i" >> matrix-timelog.txt
+        echo "start $(date) $matDim $try" >> matrix-timelog.txt
+        echo ""$matDim"x"$matDim" matrix, try $try"
+        taskset 0x00000001 ./matrix-c.o $matDim $cycles
+        echo "start $(date) $matDim $try" >> matrix-timelog.txt
     done 
 done
 #python3 matrix-plot.py matrix-output.txt matrix-plot.png
