af0d9ff136e9a26511bd817373fdf01976c48452
susloparov <mvsusloparov@edu.hse.ru>
2019-04-25 16:19:16 +0300

    Plot scripts now processor-independent

--- matrix-plot-log.py
+++ matrix-plot-log.py
@@ -10,36 +10,34 @@ import math
 filePath = sys.argv[1] # path to text file
 nameImage = sys.argv[2] # name of future plot image
 
-# 0=dimension, 1=cycles, 2=time, 3=processor
-data = pd.read_csv(filePath, sep='\t', header=None, names={0, 1, 2, 3})
-
-arm_data = data.loc[data[3] == "ARMv8 Processor rev 1 (v8l)"]
-intel_data = data.loc[data[3] == "Intel(R) Core(TM) i5-7200U CPU @ 2.50GHz"]
-
 fig = plt.figure(figsize=(19.20, 10.80)) # 1920x1080 image
 ax = fig.add_subplot(1, 1, 1)
 
-for i_data in [arm_data, intel_data]:
+file = open("matrix_means+errors.txt", "w") # to clear output file
+file.close()
+
+# 0=dimension, 1=cycles, 2=time, 3=processor
+data = pd.read_csv(filePath, sep='\t', header=None, names={0, 1, 2, 3})
+
+procNames = data[3].unique() # receive all processor names
+
+for procName in procNames:
+    i_data = data.loc[data[3] == procName]
     dimensions = i_data[0].unique() # receive all dimensions
     means=[]
     errors=[]
     for dim in dimensions:
         means.append((pd.Series.mean(i_data[2].loc[i_data[0] == dim])))
         errors.append((pd.Series.std(i_data[2].loc[i_data[0] == dim])))
-
-    ax.set_xscale('log')
+    ax.set_yscale('log')
     plt.xticks(dimensions)
-    if i_data.equals(arm_data):
-        plt.errorbar(dimensions, means, yerr=errors, marker='^', label="ARMv8 Processor rev 1 (v8l)")
-    elif i_data.equals(intel_data):
-        plt.errorbar(dimensions, means, yerr=errors, marker='^', label="Intel(R) Core(TM) i5-7200U CPU @ 2.50GHz")
+    plt.errorbar(dimensions, means, yerr=errors, marker='^', label=procName)
     file = open("matrix_means+errors.txt", "a")
     i = 0
     while i < dimensions.size:
         file.write (str(dimensions[i]) + '\t' + str(means[i]) + '\t' + str(errors[i]) + '\t' + str(i_data.iloc[0][3]) + '\n')
         i+=1
     file.close()
-
 
 plt.legend()
 
